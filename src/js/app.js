e60a5ed96ed6d281223f574f9281a0d0bc6a1058
student <student@localhost>
2019-05-02 13:24:33 +0300

    html table added

--- src/js/app.js
+++ src/js/app.js
@@ -16,10 +16,10 @@ button.addEventListener('click', () => {
 
     const cat = catEl.value;
 
-    const li = document.createElement('li');
-    li.textContent = name + '  ' + cat + '  ' + price;
-    li.className = 'list-group-item';
-    list.appendChild(li);
+    // const li = document.createElement('li');
+    // li.textContent = name + '  ' + cat + '  ' + price;
+    // li.className = 'list-group-item';
+    // list.appendChild(li);
 
     const tr = document.createElement('tr');
     tr.idName = 'tr';
@@ -27,11 +27,33 @@ button.addEventListener('click', () => {
     console.log(tr);
     console.log(table);
 
-    const td = document.getElementById('tr');
+    // const td = document.getElementById('tr');
 
     const tName = document.createElement("td");
     tName.textContent = name;
-    td.appendChild(tName);
+    tr.appendChild(tName);
+
+    const tCat = document.createElement('td');
+    tCat.textContent = cat;
+    tr.appendChild(tCat);
+
+    const tPrice = document.createElement('td');
+    tPrice.textContent = price;
+    tr.appendChild(tPrice);
+
+    const tButton = document.createElement('button');
+    tButton.textContent = 'x';
+    tButton.className = "btn btn-warning btn-sm";
+    tr.appendChild(tButton);
+
+    nameEl.value ='';
+    catEl.value = '';
+    priceEl.value = '';
+
+    tButton.addEventListener('click',()=>{
+        table.removeChild(tr);
+
+    })
 });
 
 
