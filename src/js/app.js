8416f0dd835bbb505335e8c24b6795fbb7411764
student <student@localhost>
2019-05-13 00:07:39 +0300

    most exp cat added

--- src/js/app.js
+++ src/js/app.js
@@ -8,78 +8,169 @@ const total = document.getElementById('total');
 const mostExpPur = document.getElementById('mostExpPur');
 let scoreTotal = 0;
 const arr = [];
+const mostExpCat = document.getElementById('mostExpCat');
+let cp = {};
+let result = {};
+
 
 button.addEventListener('click', () => {
 
-    const ncp = {
-        name: nameEl.value,
-        cat: catEl.value,
-        price: Number(priceEl.value),
+        //создаю объект имя-категория-цена
+        const ncp = {
+            name: nameEl.value,
+            cat: catEl.value,
+            price: Number(priceEl.value),
+        };
+        arr.push(ncp); //добавляю в массив объектов
 
-    };
-    arr.push(ncp);
+        const sorted = arr.slice(); //копирую массив для сортировки
+        sorted.sort((a, b) => b.price - a.price);//сортирую по цене
+        //вывожу наибольший объект
+        mostExpPur.children[0].textContent = sorted[0].name;
+        mostExpPur.children[1].textContent = sorted[0].cat;
+        mostExpPur.children[2].textContent = sorted[0].price;
 
-    const sorted = arr.slice();
-    sorted.sort((a, b) => b.price - a.price);
-    mostExpPur.children[0].textContent = sorted[0].name;
-    mostExpPur.children[1].textContent = sorted[0].cat;
-    mostExpPur.children[2].textContent = sorted[0].price;
+        //создаю список - таблицу
+        const tr = document.createElement('tr');
+        table.appendChild(tr);//создаю новую строку в таблице
 
-    const tr = document.createElement('tr');
-    table.appendChild(tr);
+        const tName = document.createElement("td");
+        tName.textContent = ncp.name;
+        tr.appendChild(tName); //вывожу имя в строке
 
-    const tName = document.createElement("td");
-    tName.textContent = ncp.name;
-    tr.appendChild(tName);
+        const tCat = document.createElement('td');
+        tCat.textContent = ncp.cat;
+        tr.appendChild(tCat);//вывожу категорию в строке
 
-    const tCat = document.createElement('td');
-    tCat.textContent = ncp.cat;
-    tr.appendChild(tCat);
+        const tPrice = document.createElement('td');
+        tPrice.textContent = ncp.price;
+        tr.appendChild(tPrice);//вывожу цену в строке
 
-    const tPrice = document.createElement('td');
-    tPrice.textContent = ncp.price;
-    tr.appendChild(tPrice);
+        const tButton = document.createElement('button');
+        tButton.textContent = 'x';
+        tButton.className = "btn btn-warning btn-sm";
+        tr.appendChild(tButton);//создаю кнопку удаления в строке
 
-    scoreTotal = scoreTotal + ncp.price;
-    total.textContent = scoreTotal;
+        nameEl.value = catEl.value = priceEl.value = '';//очищаю поля ввода
 
-    const tButton = document.createElement('button');
-    tButton.textContent = 'x';
-    tButton.className = "btn btn-warning btn-sm";
-    tr.appendChild(tButton);
+        count.textContent = table.rows.length;//вывожу количество покупок-строк в таблице
+        scoreTotal = scoreTotal + ncp.price; //тотал увеличиваю на цену
+        total.textContent = scoreTotal;//вывожу тотал
 
-    nameEl.value = catEl.value = priceEl.value = '';
-
-    count.textContent = table.rows.length;
-
-    tButton.addEventListener('click', () => {
-
-        scoreTotal = scoreTotal - Number(tr.children[2].textContent);
-        total.textContent = scoreTotal;
-
-        arr.splice(tr.rowIndex - 1, 1);
-        console.log(arr);
-        if (arr.length > 0) {
-            const sorted = arr.slice();
-            sorted.sort((a, b) => b.price - a.price);
-            mostExpPur.children[0].textContent = sorted[0].name;
-            mostExpPur.children[1].textContent = sorted[0].cat;
-            mostExpPur.children[2].textContent = sorted[0].price;
+        //!вычисляю дорогую категорию
+        //создаю массивы из категорий
+        for (let i = 0; i < table.rows.length; i++) {
+            cp[table.rows[i].cells[1].textContent] = [];
         }
 
-        if (arr.length === 0) {
-            mostExpPur.children[0].textContent = '';
-            mostExpPur.children[1].textContent = '';
-            mostExpPur.children[2].textContent = '0';
+        //добавляю цены в массивы категорий из таблицы
+        for (let i = 0; i < table.rows.length; i++) {
+            cp[table.rows[i].cells[1].textContent].push(Number(table.rows[i].cells[2].textContent));
         }
 
-        table.removeChild(tr);
-        count.textContent = table.rows.length;
-    });
-});
+        result = {};//создаю объект, где будут храниться суммированные категории
+        //свойства объекта - вычисленные из таблицы названия категорий, значение свойств - сумма массивов категорий
+        for (let i = 0; i < table.rows.length; i++) {
+            result[table.rows[i].cells[1].textContent] = cp[table.rows[i].cells[1].textContent].reduce(function (sum, current) {
+                return sum + current
+            });
+        }
+
+        // вычисляю наибольшее значение среди свойств
+        function findMaxCat(result) {
+            let maxCat = 0;
+            for (let key in result) {
+                if (result[key] > maxCat) {
+                    maxCat = result[key];
+                }
+            }
+            return maxCat;
+        }
+
+        //вычисляю имя свойства с наибольшим значением
+        function findMaxCatName(result) {
+            let maxCat = 0;
+            let maxCatName = '';
+            for (let key in result) {
+                if (result[key] > maxCat) {
+                    maxCat = result[key];
+                    maxCatName = key;
+                }
+            }
+            return maxCatName;
+        }
+        //вывожу имя и значение максимальной категории
+        mostExpCat.children[0].textContent = findMaxCatName(result);
+        mostExpCat.children[1].textContent = findMaxCat(result);
 
 
+        tButton.addEventListener('click', () => {
 
+            scoreTotal = scoreTotal - Number(tr.children[2].textContent);//вычитаю из тотала цену удаляемой строки
+            total.textContent = scoreTotal;
 
+            arr.splice(tr.rowIndex - 1, 1);//удаляю из массива объект строки таблицы
 
+            if (arr.length > 0) {  //если массив не пустой, сортирую массив после удаления строки
+                const sorted = arr.slice();
+                sorted.sort((a, b) => b.price - a.price);
+                mostExpPur.children[0].textContent = sorted[0].name;
+                mostExpPur.children[1].textContent = sorted[0].cat;
+                mostExpPur.children[2].textContent = sorted[0].price;
+            }
 
+            if (arr.length === 0) {//если массив пустой, то обнуляю значения самой дорогой покупки
+                mostExpPur.children[0].textContent = '';
+                mostExpPur.children[1].textContent = '';
+                mostExpPur.children[2].textContent = '0';
+            }
+
+            table.removeChild(tr);// удаляю строку таблицы
+            count.textContent = table.rows.length; //обновляю значение количества покупок
+
+            //!вычисляю самую дорогую категорию
+            for (let i = 0; i < table.rows.length; i++) {
+                cp[table.rows[i].cells[1].textContent] = [];
+            }
+
+            for (let i = 0; i < table.rows.length; i++) {
+                cp[table.rows[i].cells[1].textContent].push(Number(table.rows[i].cells[2].textContent));
+            }
+
+            result = {};
+            for (let i = 0; i < table.rows.length; i++) {
+                result[table.rows[i].cells[1].textContent] = cp[table.rows[i].cells[1].textContent].reduce(function (sum, current) {
+                    return sum + current
+                });
+            }
+            console.log(result);
+
+            function findMaxCat(result) {
+                let maxCat = 0;
+                for (let key in result) {
+                    if (result[key] > maxCat) {
+                        maxCat = result[key];
+                    }
+                }
+                return maxCat;
+            }
+
+            function findMaxCatName(result) {
+                let maxCat = 0;
+                let maxCatName = '';
+                for (let key in result) {
+                    if (result[key] > maxCat) {
+                        maxCat = result[key];
+                        maxCatName = key;
+                    }
+                }
+                return maxCatName;
+            }
+
+            mostExpCat.children[0].textContent = findMaxCatName(result);
+            mostExpCat.children[1].textContent = findMaxCat(result);
+        });
+
+    }
+)
+;
--- src/js/lib.js
+++
@@ -1,6 +0,0 @@
-// export function calculateTotal(table.) {
-//     for (const i of table.rows[i].cell[2]) {
-//
-//     }
-//
-// }
