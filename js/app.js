bcd14cbae377c0de4eedc3f9fa096606233ef520
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-24 21:58:56 +0000

    Deploy azatmagdeev/purchases to github.com/azatmagdeev/purchases.git:gh-pages

--- js/app.js
+++ js/app.js
@@ -3,10 +3,4 @@ import {PurchasesWidget} from "./widget.js";
 const widgetEl = document.getElementById('widget');
 const statsEl = document.getElementById('stats');
 const widget = new PurchasesWidget(widgetEl, statsEl);
-console.log(widget);
-
-
-
-
-
-
+console.dir(widget);
